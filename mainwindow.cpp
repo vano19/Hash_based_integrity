a6ba792d1d041aaad643ba34440ddb5ef29b8ec5
Chiklikchi <IChiklikchi@kiev.luxoft.com>
2020-10-15 19:07:22 +0300

    button for showing Images

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,11 @@
-#include "mainwindow.h"
+#include "Mainwindow.h"
 #include "ui_mainwindow.h"
 #include "Trace.h"
 #include "ALGCompression.h"
 #include "ALGKey.h"
-#include "definitions.h"
+#include "Definitions.h"
+
+#include <QDir>
 
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
@@ -16,16 +18,17 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    setWindowIcon(QIcon(":/Images/Title_icon.png"));
+    setWindowIcon(QIcon(":/resources/Title_icon.png"));
     setWindowTitle("Перевірка цілісності зображення.");
 
     connect(ui->AlgCompression, SIGNAL(clicked()), this, SLOT(onALGCompressionClicked()));
     connect(ui->AlgKey, SIGNAL(clicked()), this, SLOT(onALGKeyClicked()));
-    connect(ui->StartInject, SIGNAL(clicked()), this, SLOT(onStartClicked()));
-
+    connect(ui->StartInject, SIGNAL(clicked()), this, SLOT(onStartInjectClicked()));
+    connect(ui->StartDetach, SIGNAL(clicked()), this, SLOT(onStartDetachClicked()));
+    connect(ui->ShowImage, SIGNAL(clicked()), this, SLOT(onShowImageClicked()));
     ui->progressBar->setValue(0); // 0% loading
-    ui->StartInject->setIcon(QIcon(":/Images/input.png"));
-    ui->StartDetach->setIcon(QIcon(":/Images/output.png"));
+    ui->StartInject->setIcon(QIcon(":/resources/input.png"));
+    ui->StartDetach->setIcon(QIcon(":/resources/output.png"));
 
     QString boldText = " { font-weight: bold; }";
     ui->StartInject->setStyleSheet("QPushButton" + boldText);
@@ -34,12 +37,14 @@ MainWindow::MainWindow(QWidget *parent)
     ui->AlgCompression->setStyleSheet("QRadioButton" + boldText);
     ui->AlgKey->setStyleSheet("QRadioButton" + boldText);
 
-    ui->AlgCompression->setIcon(QIcon(":/Images/compressed-file.png"));
-    ui->AlgKey->setIcon(QIcon(":/Images/key.png"));
+    ui->AlgCompression->setIcon(QIcon(":/resources/compressed-file.png"));
+    ui->AlgKey->setIcon(QIcon(":/resources/key.png"));
 
     ui->TitleKey->setStyleSheet("QLineEdit"+ boldText);
     ui->ChoosingTitle->setStyleSheet("QLineEdit"+ boldText);
     ui->PathText->setStyleSheet("QLineEdit"+ boldText);
+
+    ui->ShowImage->setIcon(QIcon(":/resources/image-file.png"));
 }
 
 MainWindow::~MainWindow()
@@ -66,7 +71,38 @@ void MainWindow::onALGKeyClicked()
     algorithm.reset(new ALGKey());
 }
 
-void MainWindow::onStartClicked()
+void MainWindow::onStartDetachClicked()
+{
+    LOG_INF("<-");
+}
+
+void MainWindow::onShowImageClicked()
+{
+    LOG_INF("<-");
+
+    QString pathToFile = ui->PathToFile->text();
+    if (pathToFile.isEmpty())
+    {
+        callMessage("Шлях до зображення порожній!");
+    }
+    else if (!showImage(pathToFile) && !showImage(QDir::currentPath() + "\"" + pathToFile))
+    {
+       callMessage("Не можна відкрити зображення!");
+    }
+}
+
+bool MainWindow::showImage(const QString& path)
+{
+    cv::Mat img = cv::imread(path.toStdString(), cv::IMREAD_COLOR);
+    if (!img.empty())
+    {
+        cv::imshow("Display window", img);
+    }
+    img.deallocate();
+    return !img.empty();
+}
+
+void MainWindow::onStartInjectClicked()
 {
     LOG_INF("<-");
 
@@ -89,7 +125,6 @@ void MainWindow::onStartClicked()
         }
         else
         {
-            //cv::imshow("Display window", img);
             algorithm->inject(key);
         }
     }
